8ac25d94182e15ad5272f5adc3e5f9a6f63cc169
Tzuchun <tzuchun900122@gmail.com>
2024-12-29 20:54:31 +0800

    test

--- Project_PicturesGalleryPlatform/obj/Release/net8.0/Project_PicturesGalleryPlatform.AssemblyInfo.cs
+++ Project_PicturesGalleryPlatform/obj/Release/net8.0/Project_PicturesGalleryPlatform.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_PicturesGalleryPlatform")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780798a99e7d0a1aaacc1fd750e76193c72365d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_PicturesGalleryPlatform")]
