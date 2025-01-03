daae48de598192c6e4522c6ccf41a18438d186b9
Garrett86 <89382061+Garrett86@users.noreply.github.com>
2025-01-03 14:55:44 +0800

    評分功能

--- Project_PicturesGalleryPlatform/Models/Rating.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project_PicturesGalleryPlatform.Models
-{
-    public class Rating
-    {
-        public int pictureId { get; set; }
-        public string userAccount { get; set; }
-        public byte score { get; set; }
-    }
-}
